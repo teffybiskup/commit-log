486d3a42e4dc019285b627c96363d1ee150cc4df
Stefany Biskup <33550545+teffybiskup@users.noreply.github.com>
2022-11-19 12:00:07 -0800

    Create Branch services to get all branches

---
+++ src/services/SideBar/BranchTreeViewService.tsx
@@ -0,0 +1,34 @@
+import { IBranch } from '../../common/types';
+
+export async function getAllBranches(): Promise<IBranch[] | []> {
+    try {
+        return [
+            {
+                name: "local",
+                list: [
+                    "dev",
+                    "master",
+                    "feature/test",
+                    "feature/test2",
+                ],
+            },
+            {
+                name: "remote",
+                list: {
+                    name: "origin",
+                    list: [
+                        "dev",
+                        "master",
+                        "feature/test",
+                        "feature/test2",
+                        "bug/1023-fixes-issue",
+                        "bug/1024-fixes-issue",
+                        "bug/1424-fixes-issue",
+                    ],
+                },
+            },
+        ];
+    } catch (error) {
+        return [];
+    }
+}
