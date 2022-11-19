c24c696cc169bbdff06a61087ae2f72156041d25
Stefany Biskup <33550545+teffybiskup@users.noreply.github.com>
2022-11-19 14:53:29 -0800

    Update Filter Toolbar CSS

--- src/components/FilterToolbar.tsx
+++ src/components/FilterToolbar.tsx
@@ -10,6 +10,7 @@ const FilterToolbar = ({ onFilterChange }: IFilterToolbar) => {
     return (
         <div className="toolbar">
             <input
+                className="input-search"
                 onChange={handleChange}
                 placeholder="Filter by commit message..."
             >
