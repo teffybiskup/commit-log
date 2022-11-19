c359183ef70eb06b149a9e3e96afc07b726c2111
Stefany Biskup <33550545+teffybiskup@users.noreply.github.com>
2022-11-19 12:00:34 -0800

    Get all branches from service and show them properly

--- src/components/BranchTreeView.tsx
+++ src/components/BranchTreeView.tsx
@@ -1,15 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { IBranch } from '../common/types';
+import { getAllBranches } from "../services/SideBar/BranchTreeViewService";
 import "../styles/BranchTreeView.css";
 
 const BranchTreeView = () => {
+    const [branchList, setBranchList] = useState<IBranch[] | []>([]);
+
+    useEffect(() => {
+        async function fetchBranchList() {
+            const branches = await getAllBranches();
+            setBranchList(branches);
+        }
+
+        fetchBranchList();
+    }, []);
+
+    const renderBranchTreeItem = (list: string[]) => {
+        return <ul>
+            {list.map((branch) => (
+                <li title={branch}>{branch}</li>
+            ))}
+        </ul>
+    }
+
     return (
         <div className="tree-view">
-            <h4 title="Branches">Branches</h4>
-            <ul>
-                <li title="dev">dev</li>
-                <li title="master">master</li>
-                <li title="feature/test">feature/test</li>
-                <li title="feature/test2">feature/test2</li>
-            </ul>
+            {branchList.map((branch) => (
+                <>
+                    <h4 title={branch.name} className="branch-title">{branch.name} Branches</h4>
+                    {(branch.list instanceof Array) ? (
+                        <>{renderBranchTreeItem(branch.list)}</>
+                    ) : (
+                        <ul>
+                            <li title={branch.list.name}>{branch.list.name}</li>
+                            {renderBranchTreeItem(branch.list?.list as string[])}
+                        </ul>
+                    )}
+                </>
+            ))}
         </div>
     );
 };
