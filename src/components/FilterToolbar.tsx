f5fc0745245a66a25410408190ec959051bc557a
Stefany Biskup <33550545+teffybiskup@users.noreply.github.com>
2022-11-20 18:52:12 -0800

    Update FilterToolbar.tsx

--- src/components/FilterToolbar.tsx
+++ src/components/FilterToolbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { IFilterToolbar } from "../types/filter";
 import "../styles/FilterToolbar.css";
